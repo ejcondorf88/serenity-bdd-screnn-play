85fa614e9e268e3c7dbd21a3e7d442bd0997e5f3
ejcondorf88 <eliancondor2523658@gmail.com>
2026-03-30 10:30:02 -0500

    feat: ✨ add initial Serenity BDD template

---
+++ src/test/java/screenplay/runner/TestRunner.java
@@ -0,0 +1,15 @@
+package screenplay.runner;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "screenplay.stepdefinitions",
+        plugin = {"pretty"},
+        tags = "@e2e"
+)
+public class TestRunner {
+}
